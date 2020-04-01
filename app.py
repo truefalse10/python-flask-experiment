1da9f15bc6cba87eacfba988c6db329fa0de1500
Sebastian Boolean <truefalse@posteo.de>
2020-04-01 11:23:02 +0200

    simple upload form

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template, flash
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # execute julias function and return image url
-    return "Hello, World!"
+
+    if request.method == 'POST':
+        print('form submittet')
+        print(request.form['upload'])
+        flash('Your form was submittet')
+        # execute julias function and return image url
+    
+    return render_template('index.html');
 
 if __name__ == '__main__':
     app.run(debug=True)
