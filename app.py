f3726b503ee79ee5c15903801767a1c36be5920b
Sebastian Boolean <truefalse@posteo.de>
2020-04-01 11:41:53 +0200

    handle upload of file

--- app.py
+++ app.py
@@ -1,19 +1,53 @@
-from flask import Flask, request, render_template, flash
+import os
+from flask import Flask, request, render_template, flash, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
 app = Flask(__name__)
+
+# server configuration
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# helper mehtod to check if filetype is allowed
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'POST':
-        print('form submittet')
-        print(request.form['upload'])
-        flash('Your form was submittet')
-        # execute julias function and return image url
-    
-    return render_template('index.html');
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # do julias function, classify image in folder /uploads
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
